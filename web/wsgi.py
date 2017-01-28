2a680a006022be0fa1f39d7329d2b99e11bbab70
Andrew Sun <0xandrewsun@gmail.com>
2017-01-28 11:04:24 -0600

    Fix error case

--- web/wsgi.py
+++ web/wsgi.py
@@ -5,12 +5,16 @@ import vnaas
 
 PORT = 8080
 
-if __name__ == "__main__":
+def main():
     if len(sys.argv) == 1:
         port = PORT
     elif len(sys.argv) == 2:
         port = int(sys.argv[1])
     else:
         print("usage: wsgi.py [port]")
+        return
     http_server = gevent.wsgi.WSGIServer(("0.0.0.0", port), vnaas.app)
     http_server.serve_forever()
+
+if __name__ == "__main__":
+    main()
